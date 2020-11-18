5d8d82a1241fe0a41101b2b419b90cbc7c6b00c6
githubmave <lidanmei@yahoo.com>
2020-11-18 20:55:37 +1300

    add quesbook tbl

---
+++ data/quesbook.jsx
@@ -0,0 +1,33 @@
+const quesbook = {
+
+            "Lucky date" :  {
+
+                    alAnsws:  [
+                                "Nov 15",
+                                "mobile",
+                                "goat"
+
+                    ]
+            },
+            
+
+            "Found with" : {
+
+                    alAnsws:  [
+                                "Dec 25",
+                                "headphone",
+                                "dog"
+                    ]
+            },
+
+            "First Pet" : {
+
+                    alAnsws:  [
+                                "Dec 25",
+                                "headphone",
+                                "dog"
+                    ]
+            }
+
+}//quesbook
+export default quesbook
