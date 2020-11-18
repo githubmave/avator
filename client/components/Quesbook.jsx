c297eced0fca9f1a8ba8d906dde639eaf108124a
githubmave <lidanmei@yahoo.com>
2020-11-19 08:10:25 +1300

    start new nav 2

---
+++ client/components/Quesbook.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import bookData from '../../data/answbook'
+
+import {Link} from 'react-router-dom'
+
+import replList from '../../data/repliers'
+import answsArr from '../../data/answs'
+import quesList from '../../data/quess'
+import quBookData from '../../data/quesbook'
+
+
+const Quesbook = (props) => {
+
+     //let answList = Object.keys(bookData)
+ 
+  return(
+     <div className="beer">   
+       
+        
+        
+
+        <ul  className="name">
+            {quesList.map((ques,i)=>{
+             
+                return <li className="country" key={i}><Link to = {`/answList/${ques}`}>{ques}</Link></li>
+
+            })}                
+
+         </ul>
+
+
+    </div>
+      
+   )
+}
+
+export default Quesbook
